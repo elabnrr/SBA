36e94bffad250c8c5b2d418a1230267e2f469269
thuyk <ex@gmail.com>
2026-05-05 16:33:43 +0200

    Remove syntax errors

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ class TestFinance(unittest.TestCase):
     def test_calculate_savings(self):
         self.assertEqual(calculate_savings(100, 50), 150)
 
-def test_negative_values(self):
+    def test_negative_values(self):
         """Function should reject negative inputs."""
         with self.assertRaises(ValueError):
             calculate_savings(-100, 50)
